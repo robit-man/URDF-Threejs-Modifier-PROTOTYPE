936f86bd017ad96bb9de5474fbe9650d842e2d7d
Your Name <you@example.com>
2024-05-17 18:54:54 +0000

    updated index

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,7 @@ export default Object.entries(files).map(([name, file]) => {
 
             name,
             extend: true,
-            format: 'umd',
+            format: 'example',
             file: outputPath,
             sourcemap: true,
 
